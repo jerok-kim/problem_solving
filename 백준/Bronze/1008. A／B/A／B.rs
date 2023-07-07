19190c802b5c036697d3179c41ee1fa050a588b2
jerok-kim <jerok.kim@gmail.com>
2023-07-07 15:30:00 +0900

    [Bronze V] Title: A/B, Time: 4 ms, Memory: 13176 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn input_integers() -> Vec<i32> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn main() {
+    let nums = input_integers();
+    println!("{}", nums[0] as f64 / nums[1] as f64);
+}
