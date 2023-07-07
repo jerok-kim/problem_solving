2e289da537ed4eee642b31e5bfaae52ed0f9aac4
jerok-kim <jerok.kim@gmail.com>
2023-07-07 22:46:25 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 0 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.rs
@@ -0,0 +1,54 @@
+// fn main() {
+//     let input = "01111";
+//     match i32::from_str_radix(&input, 2) {
+//         Ok(res) => println!("{:b}", res),
+//         Err(_) => println!("error"),
+//     }
+// }
+
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (num1, num2) = (scan.token::<String>(), scan.token::<String>());    
+    let num1 = i128::from_str_radix(&num1, 2).ok().unwrap();
+    let num2 = i128::from_str_radix(&num2, 2).ok().unwrap();
+
+    writeln!(out, "{:b}", num1 + num2).unwrap();
+}
