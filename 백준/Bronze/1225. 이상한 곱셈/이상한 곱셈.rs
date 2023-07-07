9f389035854fd946e81bcfa35eafea690b319905
jerok-kim <jerok.kim@gmail.com>
2023-07-07 22:03:17 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 60 ms, Memory: 13316 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.rs
@@ -0,0 +1,58 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let a = scan.token::<String>();
+    let b = scan.token::<String>();
+
+    let a: Vec<char> = a.chars().collect();
+    let b: Vec<char> = b.chars().collect();
+
+    let mut result: u64 = 0;
+
+    for i in &a {
+        for j in &b {
+            let num1 = (*i as u8) - ('0' as u8);
+            let num2 = (*j as u8) - ('0' as u8);
+            result += num1 as u64 * num2 as u64;
+        }
+    }
+
+    writeln!(out, "{}", result).unwrap();
+}
