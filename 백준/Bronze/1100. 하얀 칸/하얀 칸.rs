0eec7505e34daf2feee7ec5b0c187a2f763ebee6
jerok-kim <jerok.kim@gmail.com>
2023-07-07 16:35:49 +0900

    [Bronze II] Title: 하얀 칸, Time: 0 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.rs
@@ -0,0 +1,58 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut count = 0;
+    let mut board = vec![];
+
+    for _ in 0..8 {
+        let chars_vec: Vec<char> = scan.token::<String>().chars().collect();
+        board.push(chars_vec);
+    }
+
+    for i in 0..8 {
+        for j in 0..8 {
+            if (board[i][j] == 'F') && ((i + j) % 2 == 0) {
+                count += 1;
+            }
+        }
+    }
+    
+    writeln!(out, "{}", count).unwrap();
+}
