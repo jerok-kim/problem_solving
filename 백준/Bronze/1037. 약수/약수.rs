eb4655e8d89f8743e70d3edf46f53848467219a1
jerok-kim <jerok.kim@gmail.com>
2023-07-07 15:26:38 +0900

    [Bronze I] Title: 약수, Time: 4 ms, Memory: 13156 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.rs
+++ 백준/Bronze/1037. 약수/약수.rs
@@ -1,53 +1,24 @@
-use std::{io, str};
-use io::Write;
+use std::io;
 
-pub struct UnsafeScanner<R> {
-    reader: R,
-    buf_str: Vec<u8>,
-    buf_iter: str::SplitAsciiWhitespace<'static>,
-}
+fn input_integers() -> Vec<i32> {
+    let mut s = String::new();
 
-impl<R: io::BufRead> UnsafeScanner<R> {
-    pub fn new(reader: R) -> Self {
-        Self {
-            reader,
-            buf_str: vec![],
-            buf_iter: "".split_ascii_whitespace(),
-        }
-    }
+    io::stdin().read_line(&mut s).unwrap();
 
-    pub fn token<T: str::FromStr>(&mut self) -> T {
-        loop {
-            if let Some(token) = self.buf_iter.next() {
-                return token.parse().ok().expect("Failed parse");
-            }
-            self.buf_str.clear();
-            self.reader
-                .read_until(b'\n', &mut self.buf_str)
-                .expect("Failed read");
-            self.buf_iter = unsafe {
-                let slice = str::from_utf8_unchecked(&self.buf_str);
-                std::mem::transmute(slice.split_ascii_whitespace())
-            }
-        }
-    }
+    let values = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
 }
 
 fn main() {
-    let (stdin, stdout) = (io::stdin(), io::stdout());
-    let mut scan = UnsafeScanner::new(stdin.lock());
-    let mut out = io::BufWriter::new(stdout.lock());
-
-    let n = scan.token::<usize>();
-    let mut nums = Vec::new();
-
-    for _ in 0..n {
-        nums.push(scan.token::<i64>());
-    }
+    let _t = input_integers();
+    let mut nums = input_integers();
 
     nums.sort();
 
-    let ret = nums.first().unwrap() * nums.last().unwrap();
-
-    writeln!(out, "{}", ret).unwrap();
+    println!("{}", nums.first().unwrap() * nums.last().unwrap());
 }
