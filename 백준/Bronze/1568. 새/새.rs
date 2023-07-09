ee2ce814049164cc9edea11cab808f1373011ad4
jerok-kim <jerok.kim@gmail.com>
2023-07-09 17:03:12 +0900

    [Bronze II] Title: 새, Time: 4 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/1568. 새/새.rs
@@ -0,0 +1,57 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut n = scan.token::<u64>();
+    let mut second = 0;
+    let mut natural_num = 0;
+
+    loop {
+        if n == 0 { break; }
+        second += 1;
+        if n < natural_num + 1 {
+            natural_num = 1;
+        } else {
+            natural_num += 1;
+        }
+        n = n - natural_num;
+    }
+
+    writeln!(out, "{}", second).unwrap();
+}
